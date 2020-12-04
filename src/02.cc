33fbdd23ba96ca4041b6834ad34af419f3c1d9ee
MatthewRasa <matt@raztech.com>
2020-12-03 22:47:41 -0500

    Added day 2 solutions

---
+++ src/02.cc
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <functional>
+#include <sstream>
+#include <cstring>
+
+struct Input {
+	std::size_t start, end;
+	char c;
+	std::string password;
+};
+
+static Input parse_input_line(std::string line) {
+	Input input;
+	std::string range, cstr, password;
+	std::istringstream ss{std::move(line)};
+	ss >> range >> cstr >> input.password;
+
+	std::istringstream rss{range};
+	std::string sstart, send;
+	std::getline(rss, sstart, '-');
+	std::getline(rss, send, '-');
+
+	input.start = std::stoul(sstart);
+	input.end = std::stoul(send);
+	input.c = cstr[0];
+	return input;
+}
+
+static bool part1_is_valid(std::size_t start, std::size_t end, char c, const std::string &password) {
+	std::size_t count = std::count(password.begin(), password.end(), c);
+	return start <= count && count <= end;
+}
+
+static bool part2_is_valid(std::size_t start, std::size_t end, char c, const std::string &password) {
+	return (password[start - 1] == c) != (password[end - 1] == c);
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <1|2>" << std::endl;
+		return 1;
+	}
+	std::function<bool(std::size_t, std::size_t, char, const std::string &)> is_valid_func;
+	if (strncmp(argv[1], "1", 1) == 0) {
+		is_valid_func = part1_is_valid;
+	} else if (strncmp(argv[1], "2", 1) == 0) {
+		is_valid_func = part2_is_valid;
+	} else {
+		std::cerr << argv[0] << ": invalid argument '" << argv[1] << "'" << std::endl;
+		return 1;
+	}
+
+	std::size_t count = 0;
+	for (std::string line; std::getline(std::cin, line); ) {
+		auto input = parse_input_line(std::move(line));
+		count += is_valid_func(input.start, input.end, input.c, input.password);
+	}
+	std::cout << count << std::endl;
+	return 0;
+}
